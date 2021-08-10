8712d9746b9e2968fbd0d5a9f0b9777353a7bcbb
Caddox <rjsalkind@gmail.com>
2021-08-10 14:30:22 -0700

    Normal comment to doc comment

--- db.py
+++ db.py
@@ -30,5 +30,7 @@ def init_app(app):
     app.teardown_appcontext(close_db)
 
 def query_builder(head, rep, num_items, end=''):
-    # Make a query string with a given head, repeating part, and the number of items that will be inserted
+    '''
+    Make a query string with a given head, repeating part, and the number of items that will be inserted
+    '''
     return head + rep * num_items + end
